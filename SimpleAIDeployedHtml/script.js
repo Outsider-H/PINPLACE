699bad738374dfe8967b3a3cda4c511033eeac28
wldyd423 <69039188+wldyd423@users.noreply.github.com>
2021-10-28 16:09:28 +0900

    Added Simple AI Page

---
+++ SimpleAIDeployedHtml/script.js
@@ -0,0 +1,50 @@
+let model;
+const loadModel = async () => {
+  model = await tf.loadLayersModel("model/place/model.json");
+};
+loadModel();
+
+const imgSelector = document.querySelector("#img");
+const predictBtn = document.querySelector(".predict");
+
+imgSelector.addEventListener("change", () => {
+  document.querySelector(".imageBox").innerHTML = "";
+  let reader = new FileReader();
+  reader.addEventListener("load", () => {
+    let dataURL = reader.result;
+    let img = document.createElement("img");
+    img.style.height = "200px";
+    img.src = dataURL;
+    img.classList.add("curImg");
+    document.querySelector(".imageBox").appendChild(img);
+  });
+  let file = imgSelector.files[0];
+  reader.readAsDataURL(file);
+});
+
+predictBtn.addEventListener("click", async () => {
+  let imgInput = document.querySelector(".curImg");
+  const preprocess = (img) => {
+    let tensor = tf.browser
+      .fromPixels(img)
+      .resizeNearestNeighbor([128, 128])
+      .toFloat();
+    return tensor.div(255).expandDims();
+  };
+  let tensor = preprocess(imgInput);
+  let prediction = await model.predict(tensor).data();
+  let top = Array.from(prediction)
+    .map((prob, idx) => {
+      return {
+        probability: prob,
+        className: IMAGENET_CLASSES2[idx],
+      };
+    })
+    .sort((a, b) => {
+      return b.probability - a.probability;
+    })
+    .slice(0, 1);
+  console.log(top);
+  const pred = document.querySelector(".result");
+  pred.innerHTML = `${top[0].className}`;
+});
