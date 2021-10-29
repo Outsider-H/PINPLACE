54c3cd896df65d5bc93c65e0fcf385d133c8b13f
wldyd423 <69039188+wldyd423@users.noreply.github.com>
2021-10-29 21:34:28 +0900

    sql script added

--- BackEnd/Prototype/mysql/script.sql
+++ BackEnd/Prototype/mysql/script.sql
@@ -12,3 +12,17 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`username`, `password`, `isAdmin`) VALUES ("admin", "admin", "1");
 INSERT INTO `user` (`username`, `password`, `isAdmin`) VALUES ("testid", "testpw","0");
+
+CREATE TABLE `text` (
+    `idtext` INT NOT NULL AUTO_INCREMENT,
+    `iduser` INT NOT NULL,
+    `content` VARCHAR(500) NOT NULL,
+    PRIMARY KEY(`idtext`)
+);
+
+CREATE TABLE `image` (
+    `idimage` INT NOT NULL AUTO_INCREMENT,
+    `iduser` INT NOT NULL,
+    `image` VARCHAR(500) NOT NULL,
+    PRIMARY KEY(`idimage`)
+);
