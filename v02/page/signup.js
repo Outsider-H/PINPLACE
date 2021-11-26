e6c8959f3043488d0d429e34a9d88d64fcc4b763
wldyd423 <69039188+wldyd423@users.noreply.github.com>
2021-11-26 11:44:57 +0900

    Fixed Naming
    index.html is first page when entering http://localhost:8898 therefore start1 -> index, index -> predict
    after index->start2->userguide->loginpage ->predict So the flow would seem more natural.

--- v02/page/signup.js
+++ v02/page/signup.js
@@ -9,34 +9,38 @@ form.addEventListener("submit", (event) => {
   console.log(data);
   console.log(data.get("id"));
   console.log(data.get("name"));
-  
+
   //add additional content here
   if (data.get("password") !== data.get("pwagain")) {
-      alert("Password is not equal.");
-      return;
+    alert("Password is not equal.");
+    return;
   }
 
   const xhr = new XMLHttpRequest();
   xhr.open("POST", "/signupform");
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhr.send(`id=${data.get("id")}&name=${data.get("name")}&password=${data.get("password")}`);
+  xhr.send(
+    `id=${data.get("id")}&name=${data.get("name")}&password=${data.get(
+      "password"
+    )}`
+  );
   xhr.onload = () => {
-      console.log(xhr.responseText);
+    console.log(xhr.responseText);
   };
   xhr.onreadystatechange = () => {
-      console.log(xhr.respose);
+    console.log(xhr.respose);
     window.location.href = "/signupSucceed.html";
   };
 });
 
 window.addEventListener("load", (event) => {
-    form.reset();
+  form.reset();
 });
 
 form.addEventListener("change", (event) => {
-    event.preventDefault();
-    let data = new FormData(form);
-    console.log(data);
-    console.log(data.get("id"));
-    console.log(data.get("name"));
+  event.preventDefault();
+  let data = new FormData(form);
+  console.log(data);
+  console.log(data.get("id"));
+  console.log(data.get("name"));
 });
