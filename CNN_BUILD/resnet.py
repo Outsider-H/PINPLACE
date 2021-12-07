61249fde48758d19aefcc985886485f2f3495c6b
orioncsy <90237119+orioncsy@users.noreply.github.com>
2021-12-07 20:40:22 +0900

    Update resnet.py

--- CNN_BUILD/resnet.py
+++ CNN_BUILD/resnet.py
@@ -1,16 +1,20 @@
+#google colab으로 파일 디렉터리 연결
 from google.colab import drive
 drive.mount('/content/gdrive')
 
-
+#데이터 전처리 및 파일 업로드
+#파일사이즈는 128*128 크기로 줄여서 입력하였고 카테고리로 10가지 장소을 입력하여 npy로 저장
 #데이터 전처리 및 파일 업로드
 #파일사이즈는 128*128 크기로 줄여서 입력하였고 카테고리로 10가지 장소을 입력하여 npy로 저장
 from PIL import Image
 import os, glob, numpy as np
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
-caltech_dir = "/content/gdrive/MyDrive/images_s"
+caltech_dir = "/content/gdrive/MyDrive/images_s4"
 categories = ["Dongdaemun_Design_Plaza", "Gyeongui_Line_Forest_Park", "Naksan_Park", "Namsan_Seoul_Tower","The_Hyundai_Seoul_Mall", 
               "Myeongdong_Cathedral", "Ikseon_Dong_Hanok_Village", "Jamsil_Lotte_Tower", "Han_River_Sebitseom", "Haebangchon"]
 nb_classes = len(categories)
@@ -45,9 +49,11 @@ y = np.array(y)
 
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 xy = (X_train, X_test, y_train, y_test)
-np.save("/content/gdrive/MyDrive/image_sdata_gen4.npy", xy)
+
+#data set을 npy형태로 
+np.save("/content/gdrive/MyDrive/image_data.npy", xy)
 
 #npy파일을 가져온다.
 import os, glob, numpy as np
@@ -65,9 +71,10 @@ session = tf.compat.v1.Session(config=config)
 
 
 
-X_train, X_test, y_train, y_test = np.load('/content/gdrive/MyDrive/image_sdata_gen.npy',allow_pickle = True)
+X_train, X_test, y_train, y_test = np.load('/content/gdrive/MyDrive/image_data.npy',allow_pickle = True)
 print(X_train.shape)
 print(X_train.shape[0])
+
 
 #일반화시켜 input값의 범위 조정
 categories = ["Dongdaemun_Design_Plaza", "Gyeongui_Line_Forest_Park", "Naksan_Park", "Namsan_Seoul_Tower","The_Hyundai_Seoul_Mall", 
@@ -77,7 +84,9 @@ nb_classes = len(categories)
 X_train = X_train.astype(float) / 255
 X_test = X_test.astype(float) / 255
 
+#CNN모델 생성(ResNet 50 model)
 from keras.applications.resnet_v2 import ResNet50V2
+
 with tf.device('/device:GPU:0'):
   model = ResNet50V2(include_top=True, weights=None, input_shape=(128,128,3), classes=10)
   model.compile(loss='categorical_crossentropy', optimizer='Nadam', metrics=['accuracy'])
@@ -89,14 +98,20 @@ with tf.device('/device:GPU:0'):
   model_path = model_dir + '/multi_img_classification.model'
   checkpoint = ModelCheckpoint(filepath=model_path , monitor='val_loss', verbose=1, save_best_only=True)
   early_stopping = EarlyStopping(monitor='val_loss', patience=6)
-  
-  with tf.device('/device:GPU:0'):
-  history = model.fit(X_train, y_train, batch_size=32, epochs=50, validation_split=0.2)
-  
-  #모델 정확도 출력
+
+#모델 형태를 표로 요약
+model.summary()
+
+#모델 학습하기
+
+with tf.device('/device:GPU:0'):
+  history = model.fit(X_train, y_train, batch_size=32, epochs=10, validation_split=0.2)
+
+#모델 정확도 출력
 print("정확도 : %.4f" % (model.evaluate(X_test, y_test)[1]))
 
-model.save('/content/gdrive/MyDrive/place_rec.h5')
+#모델 저장하기
+model.save('/content/gdrive/MyDrive/resnet.h5')
 
 import matplotlib.pyplot as plt
 # summarize history for accuracy
